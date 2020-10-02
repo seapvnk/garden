6c5579b58f7823d5b78f0b542277a981bd17cc8f
Pedro Sérgio <www.psro@gmail.com>
2020-10-02 15:35:10 -0300

    garden created

--- garden.php
+++ garden.php
@@ -1,10 +1,47 @@
 <?php
 
+$GARDEN_SCOPE = true;
 require_once './config/config.php';
 
 Loader::include('GardenIO');
 
-GardenIO::print('Undefined something at line '. __LINE__, G_ERROR);
-GardenIO::print('Take care of line '. __LINE__, G_WARNING);
-GardenIO::print('Seeds created!', G_SUCCESS);
-GardenIO::print('Usage: php garden.php seed Users', G_INFO);
+$options = [
+    'help/list' => 'show all commands',
+    'model' => [
+        '=delete' => 'delete a model',
+        '=create' => 'create a model',
+    ],
+    'controller' => [
+        '=delete' => 'delete a controller',
+        '=create' => 'create a controller',
+    ],
+];
+
+if (GardenIO::inputCount() === 0) {
+    echo GardenIO::print('garden require at least one arg... Try "php garden.php help"', G_INFO);
+}
+
+if (GardenIO::has('help') || GardenIO::has('list')) {
+    if (GardenIO::has('help')) {
+        $output = "Available commands";
+    } else {
+        $output = "List of commands";
+    }
+
+    $output .= GardenIO::EOL;
+
+    foreach ($options as $option => $usage) {
+        if (is_array($usage)) {
+            $usageString = GardenIO::_TAB . "$option :" . GardenIO::EOL;
+            foreach ($usage as $opt => $description) {
+                $usageString .= str_repeat(GardenIO::_TAB, 2) . "{$opt} : {$description} " . GardenIO::EOL;
+            }
+            $usage = $usageString;
+            $output .= $usage;
+        } else {
+            $output .= GardenIO::_TAB . "{$option} : {$usage}" . GardenIO::EOL;
+        }
+    }
+
+    GardenIO::print($output, G_INFO);
+}

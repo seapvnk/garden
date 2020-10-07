26f94e0f584eb09806f14b25b13dea321013231a
Pedro Sérgio <www.psro@gmail.com>
2020-10-07 20:16:55 -0300

    Initial view using 'view' method

--- index.php
+++ index.php
@@ -6,15 +6,9 @@ Loader::include('Router');
 $routes = new Router;
 
 $routes->bind('', function($action, $params) {
-    echo "
-        <p align='center' style='width: 40vw; margin: 55px auto;'>
-            <img width='100%' src='https://engineering.giphy.com/wp-content/uploads/2018/02/ai.gif'>
-        </p>
-        <p align='center' style='font-family: monospace; font-size: 16px; letter-spacing:6px'> 
-            Happy gardening!
-        </p>
-    ";
-
+    view('welcome', [
+        'welcomename' => 'WORKING!!!'
+    ]);
 });
 
 $routes->dispatch();
---
+++ src/view/welcome.garden.php
@@ -0,0 +1,5 @@
+<p align="center" style="width: 40vw; margin: 55px auto;">
+    <img width="100%" src="https://engineering.giphy.com/wp-content/uploads/2018/02/ai.gif" alt="">
+</p>
+
+<p align="center" style="width: 40vw; margin: 55px auto;"> Happy gardening! </p>

a2e170ada204e910a456d520ce710a21f72692b6
Pedro <36551559+seapvnk@users.noreply.github.com>
2020-10-09 13:38:59 -0300

    Default 404 view

---
+++ src/view/404.garden.php
@@ -0,0 +1,14 @@
+<body style="background-color: #f4f4f4">
+    <h1 
+        align="center" 
+        style="margin-top:35vh; font-size: 5rem; font-family: monospace; color: rgba(0,0,0, .3)"]
+    >
+        404
+    </h1>
+    <p
+        align="center"
+        style="font-size: 1rem; font-family: monospace; color: rgba(0,0,0, .3)"]
+    >
+        This page doesn't exists
+    </p>
+</body> 
